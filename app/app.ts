cfd3419b7ea013370d7fba1aca0f61246f73fc64
Alcalixo <rodrix_gonzalo@outlook.com>
2024-02-28 11:44:20 -0300

    Modificación del TS y su correspondiente volcado en JS

--- app/app.ts
+++ app/app.ts
@@ -5,16 +5,40 @@ console.log(message);
 let cantidad: any = 4;
 let dec: any[] = [1, true, "verde"];
 
+/*string*/
 let saludo: string = "hola, mundo";
 let nombre: string = "Gonzalo";
 let mensaje: string = `Hola, soy ${nombre}, y soy nuevo en TypeScript`;
 console.log(mensaje);
 
+/*number*/
 let codigoProducto: number = 6;
 let ivaProducto: number = 10.5;
 
+/*boolean*/
 let estadoProducto: boolean = true;
 
+/*void*/
 function mensajeUsuario(): void {
   console.log("Este es un mensaje para tí, usuario");
 }
+
+/*enum*/
+enum color {
+  Blanco,
+  Rojo,
+  Azul,
+  Amarillo,
+}
+
+let colorAuto: color = color.Azul;
+console.log(colorAuto);
+
+enum contratos{
+    Permanente,
+    Parcial,
+    Prueba
+}
+
+let estadoContrato:contratos=contratos.Parcial;
+console.log(estadoContrato);
--- output/app.js
+++ output/app.js
@@ -4,13 +4,35 @@ console.log(message);
 /*tipo de variable*/
 let cantidad = 4;
 let dec = [1, true, "verde"];
+/*string*/
 let saludo = "hola, mundo";
 let nombre = "Gonzalo";
 let mensaje = `Hola, soy ${nombre}, y soy nuevo en TypeScript`;
 console.log(mensaje);
+/*number*/
 let codigoProducto = 6;
 let ivaProducto = 10.5;
+/*boolean*/
 let estadoProducto = true;
+/*void*/
 function mensajeUsuario() {
     console.log("Este es un mensaje para tí, usuario");
 }
+/*enum*/
+var color;
+(function (color) {
+    color[color["Blanco"] = 0] = "Blanco";
+    color[color["Rojo"] = 1] = "Rojo";
+    color[color["Azul"] = 2] = "Azul";
+    color[color["Amarillo"] = 3] = "Amarillo";
+})(color || (color = {}));
+let colorAuto = color.Azul;
+console.log(colorAuto);
+var contratos;
+(function (contratos) {
+    contratos[contratos["Permanente"] = 0] = "Permanente";
+    contratos[contratos["Parcial"] = 1] = "Parcial";
+    contratos[contratos["Prueba"] = 2] = "Prueba";
+})(contratos || (contratos = {}));
+let estadoContrato = contratos.Parcial;
+console.log(estadoContrato);
